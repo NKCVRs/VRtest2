8cefbb346016fca889b1e376ef14560b7f14fb39
kamosica <nkc.kenta.oishi@gmail.com>
2017-11-07 14:00:51 +0900

    通信テスト

---
+++ Assets/oishi/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player : NetworkBehaviour
+{
+    public GameObject Sphere;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CmdSphere();
+            }
+
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * 0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.forward * -0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.right * -0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * 0.2f;
+        }
+    }
+
+    [Command]
+    void CmdSphere()
+    {
+        GameObject obj = (GameObject)Instantiate(Sphere, transform.position, transform.rotation);
+        NetworkServer.Spawn(obj);
+    }
+}
