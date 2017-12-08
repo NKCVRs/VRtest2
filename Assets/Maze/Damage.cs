0c962ee296276d4bb2b4441cd2ab9c2053c9da83
hiramatsuckn <nkchiramatsuryouya@gmail.com>
2017-12-08 15:48:41 +0900

    trapとダメージ

---
+++ Assets/Maze/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Damage : NetworkBehaviour {
+
+    [SerializeField]
+    private int damage=30;
+    public void setDamage(int Dam) { damage = Dam; }
+    public int getDamage() { return damage; }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
