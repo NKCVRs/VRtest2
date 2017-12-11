be70b20b3eb2bd7d27d72f289f297446ad0abceb
hiramatsuckn <nkchiramatsuryouya@gmail.com>
2017-12-11 15:01:48 +0900

    トラップ関連

---
+++ Assets/Maze/Attacks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour {
+    public float attack=34;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            Status stat = col.gameObject.GetComponent<Status>();
+            stat.Damages(attack);
+        }
+    }
+}
---
+++ Assets/Maze/Status.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+    public float CharacterHP;
+    [SerializeField]
+    private float maxhp;
+
+	// Use this for initialization
+	void Start () {
+        if (gameObject.tag == "Player")
+        {
+            CharacterHP = maxhp;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CharacterHP <= 0)
+        {
+            Dead();
+        }
+	}
+    public void Damages(float damage)
+    {
+        CharacterHP -= damage;
+    }
+    public void Dead()
+    {
+
+    }
+}
