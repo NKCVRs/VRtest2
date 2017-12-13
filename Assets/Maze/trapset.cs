9bfe774b04e635fb89cccbe5cbcabcf989de3e0c
hiramatsuckn <nkchiramatsuryouya@gmail.com>
2017-12-13 14:48:59 +0900

    トラップの設置

---
+++ Assets/Maze/trapset.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class trapset : NetworkBehaviour {
+
+    private int Cutterremain;
+    private int Needleremain;
+    private int Bladeremain;
+    private int Spearremain;
+    public GameObject CutterTrap;
+    public GameObject NeedleTrap;
+    public GameObject BladeTrap;
+    public GameObject SpearTrap;
+    private GameObject SetPrefab;
+    private Vector3 clickPosition;
+    Set tset;
+    enum Set
+    {
+        CUTTER,
+        NEEDLE,
+        BLADE,
+        SPEAR,
+        None
+    }
+
+	// Use this for initialization
+	void Start () {
+        Cutterremain = 5;
+        Needleremain = 5;
+        Bladeremain = 5;
+        Spearremain = 5;
+        tset = Set.None;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        keyInput();
+    }
+    void keyInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Cutter();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Needle();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Blade();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Spear();
+        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    // Vector3でマウスがクリックした位置座標を取得する
+        //    clickPosition = Input.mousePosition;
+        //    // Z軸修正
+        //    clickPosition.z = 19.5f;
+        //    // オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+        //    // ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+        //    Instantiate(SetPrefab, Camera.main.ScreenToWorldPoint(clickPosition), SetPrefab.transform.rotation);
+        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+            clickPosition.z = 19.5f;
+            Setting();
+        }
+    }
+    void Setting()
+    {
+        switch (tset)
+        {
+            case Set.CUTTER:
+                if (Cutterremain > 0)
+                {
+                    Cutterremain -= 1;
+                    Instantiate(SetPrefab, Camera.main.ScreenToWorldPoint(clickPosition), SetPrefab.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("在庫がありません");
+                }
+                break;
+            case Set.NEEDLE:
+                if (Needleremain > 0)
+                {
+                    Needleremain -= 1;
+                    Instantiate(SetPrefab, Camera.main.ScreenToWorldPoint(clickPosition), SetPrefab.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("在庫がありません");
+                }
+                break;
+            case Set.BLADE:
+                if (Bladeremain>0)
+                {
+                    Bladeremain -= 1;
+                    Instantiate(SetPrefab, Camera.main.ScreenToWorldPoint(clickPosition), SetPrefab.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("在庫がありません");
+                }
+                break;
+            case Set.SPEAR:
+                if (Spearremain > 0)
+                {
+                    Spearremain -= 1;
+                    Instantiate(SetPrefab, Camera.main.ScreenToWorldPoint(clickPosition), SetPrefab.transform.rotation);
+                }
+                else
+                {
+                    Debug.Log("在庫がありません");
+                }
+                break;
+            case Set.None:
+                Debug.Log("トラップを選んでください");
+                break;
+        }
+    }
+    public void Cutter()
+    {
+        SetPrefab = CutterTrap;
+        tset = Set.CUTTER;
+    }
+    public void Needle()
+    {
+        SetPrefab = NeedleTrap;
+        tset = Set.NEEDLE;
+    }
+    public void Blade()
+    {
+        SetPrefab = BladeTrap;
+        tset = Set.BLADE;
+    }
+    public void Spear()
+    {
+        SetPrefab = SpearTrap;
+        tset = Set.SPEAR;
+    }
+}
